8416aa5d78a90e9d67763b1879b4c45ed289aea9
Reece Devenney <reecedevenney@gmail.com>
2022-12-06 18:16:54 -0500

    testing bug fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/MERNhw',
+  process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/MERNhw',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
