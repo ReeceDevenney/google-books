d07404fbf4ba88a7d7e36fcce1641166c6a02b66
Reece Devenney <reecedevenney@gmail.com>
2022-12-07 18:32:26 -0500

    removed console.log

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -58,7 +58,6 @@ const resolvers = {
         },
 
         removeBook: async (parent, {bookId}, context) => {
-            console.log(context.user._id)
             if(context.user) {
                 const deletededBook = await User.findOneAndUpdate(
                     { _id: context.user._id },
